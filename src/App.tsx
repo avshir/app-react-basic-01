8f30552ff429a87acddfc95a95b18d84af1e9a02
avshir <av.shirinskaya@gmail.com>
2023-10-16 18:41:38 +0200

    feat: apply ModalContext in App, index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useContext } from "react";
 import CreateProduct from "./components/CreateProduct";
 import ErrorMessage from "./components/ErrorMessage";
 import Loader from "./components/Loader";
 import Modal from "./components/Modal";
-import { Product } from "./components/Product";
+import { Product } from './components/Product';
 //import { products } from './data/products';
 import { useProducts } from "./hooks/products";
+import { ModalContext } from "./context/ModalContext";
+import { IProduct } from "./models";
 
 function App() {
-  const { products, loading, error } = useProducts();
-  const [modal, setModal] = useState(false);
+  const { products, loading, error, addProduct } = useProducts();
+  const { modal, open: openModal, close: closeModal } = useContext(ModalContext);
+
+  const createHandler = (product: IProduct ) => {
+    closeModal();
+    addProduct(product);
+  }
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
@@ -23,11 +30,18 @@ function App() {
       {/* <Product product={products[0]} title={'text'} count={42} />
       <Product product={products[1]} /> */}
 
-      {modal && <Modal title="Create new product" onClose={() => setModal(false) }>
-        <CreateProduct onCreate={() => setModal(false)} />
-      </Modal>}
+      {modal && (
+        <Modal title="Create new product" onClose={closeModal}>
+          <CreateProduct onCreate={ createHandler } />
+        </Modal>
+      )}
 
-      <button className="fixed bottom-5 right-5 rounded-full bg-blue-700 text-white text-2xl px-4 py-2" onClick={()=>setModal((prev) => !prev)}>+</button>
+      <button
+        className="fixed bottom-5 right-5 rounded-full bg-blue-700 text-white text-2xl px-4 py-2"
+        onClick={openModal}
+      >
+        +
+      </button>
     </div>
   );
 }
