5abfca20668750f22fc921ce215381cafccaa61e
avshir <av.shirinskaya@gmail.com>
2023-10-18 11:32:16 +0200

    fix: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ProductsPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<p>Ups!</p>} />
       </Routes>
     </>
   );
